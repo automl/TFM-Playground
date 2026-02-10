6cb6ef371bd2ed51504a709e2442013c80c62140
Thomas Pumir <thomaspumir@Thomass-MacBook-Pro.local>
2026-02-10 19:13:40 +0100

    update prior exports

--- tfmplayground/priors/__init__.py
+++ tfmplayground/priors/__init__.py
@@ -6,16 +6,33 @@ from .dataloader import (
     TabICLPriorDataLoader,
     TICLPriorDataLoader,
     TabPFNPriorDataLoader,
+    TimeSeriesPriorDataLoader,
 )
 from .utils import build_ticl_prior, build_tabpfn_prior
+from .timeseries import (
+    TemporalXSampler,
+    TimeSeriesSCM,
+    ForecastPriorDataset,
+    DEFAULT_TS_FIXED_HP,
+    TS_PRIOR_PRESETS,
+)
 
 __version__ = "0.0.1"
 __all__ = [
+    # DataLoaders
     "PriorDataLoader", 
     "PriorDumpDataLoader",
     "TabICLPriorDataLoader",
     "TICLPriorDataLoader",
     "TabPFNPriorDataLoader",
+    "TimeSeriesPriorDataLoader",
+    # Builders
     "build_ticl_prior",
     "build_tabpfn_prior",
+    # Time series components
+    "TemporalXSampler",
+    "TimeSeriesSCM",
+    "ForecastPriorDataset",
+    "DEFAULT_TS_FIXED_HP",
+    "TS_PRIOR_PRESETS",
 ]
