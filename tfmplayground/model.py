fc9ff9367e767b5f2a07c9ef42344653af498cd8
Alexander Pfefferle <mail@alexanderpfefferle.com>
2026-04-13 19:53:27 +0200

    better naming and removing the TransformerEncoderStack class in nanoTabPFN

--- tfmplayground/model.py
+++ tfmplayground/model.py
@@ -12,7 +12,7 @@ class NanoTabPFNModel(nn.Module):
     def __init__(
         self, embedding_size: int, num_attention_heads: int, mlp_hidden_size: int, num_layers: int, num_outputs: int
     ):
-        """Initializes the feature/target encoder, transformer stack and decoder"""
+        """Initializes the feature/target encoder, transformer blocks and decoder"""
         super().__init__()
         self.embedding_size = embedding_size
         self.num_attention_heads = num_attention_heads
@@ -21,11 +21,11 @@ class NanoTabPFNModel(nn.Module):
         self.num_outputs = num_outputs
         self.feature_encoder = FeatureEncoder(embedding_size)
         self.target_encoder = TargetEncoder(embedding_size)
-        self.transformer_encoder = TransformerEncoderStack(
-            num_layers, embedding_size, num_attention_heads, mlp_hidden_size
-        )
+        for _ in range(num_layers):
+            self.transformer_blocks.append(TransformerEncoderLayer(embedding_size, num_attention_heads, mlp_hidden_size))
         self.decoder = Decoder(embedding_size, mlp_hidden_size, num_outputs)
 
+    # TODO: consider getting rid of this and just provide a single interface
     def forward(self, *args, **kwargs) -> torch.Tensor:
         """
         Provides two interfaces:
@@ -35,7 +35,7 @@ class NanoTabPFNModel(nn.Module):
                 y_train: (torch.Tensor) a tensor of shape (batch_size, num_train_datapoints, 1)
                 X_test: (torch.Tensor) a tensor of shape (batch_size, num_test_datapoints, num_features)
 
-        model((x,y), single_eval_pos)
+        model((x,y), train_test_split_index)
             Args:
                 x: (torch.Tensor) a tensor of shape (batch_size, num_datapoints, num_features)
                 y: (torch.Tensor) a tensor of shape (batch_size, num_train_datapoints, 1)
@@ -43,7 +43,7 @@ class NanoTabPFNModel(nn.Module):
 
         The former is similar to the sklearn interface.
         In the latter x is the concatenation of X_train and X_test, y is y_train and
-        single_eval_pos is the length of X_train.
+        train_test_split_index is the length of X_train.
         Our model internally works with the latter representation, so we convert the former into
         the latter and forward it to _forward.
 
@@ -56,13 +56,13 @@ class NanoTabPFNModel(nn.Module):
             x = args[0]
             if args[2] is not None:
                 x = torch.cat((x, args[2]), dim=1)
-            return self._forward((x, args[1]), single_eval_pos=args[0].shape[1], **kwargs)
+            return self._forward((x, args[1]), train_test_split_index=args[0].shape[1], **kwargs)
         elif len(args) == 1 and isinstance(args[0], tuple):
-            # case model((x,y), single_eval_pos=None)
+            # case model((x,y), train_test_split_index=None)
             return self._forward(*args, **kwargs)
 
     def _forward(
-        self, src: tuple[torch.Tensor, torch.Tensor], single_eval_pos: int, num_mem_chunks: int = 1
+        self, src: tuple[torch.Tensor, torch.Tensor], train_test_split_index: int, num_mem_chunks: int = 1
     ) -> torch.Tensor:
         x_src, y_src = src
         # we expect the labels to look like (batches, num_train_datapoints, 1),
@@ -71,7 +71,7 @@ class NanoTabPFNModel(nn.Module):
             y_src = y_src.unsqueeze(-1)
         # from here on B=Batches, R=Rows, C=Columns, E=embedding size
         # converts scalar values to embeddings, so (B,R,C-1) -> (B,R,C-1,E)
-        x_src = self.feature_encoder(x_src, single_eval_pos)
+        x_src = self.feature_encoder(x_src, train_test_split_index)
         num_rows = x_src.shape[1]
         # padds the y_train up to y by using the mean,
         # then converts scalar values to embeddings (B,R,1,E)
@@ -80,37 +80,37 @@ class NanoTabPFNModel(nn.Module):
         # to give us the full table of embeddings (B,R,C,E))
         src = torch.cat([x_src, y_src], 2)
         # repeatedly applies the transformer block on (B,R,C,E)
-        output = self.transformer_encoder(src, single_eval_pos, num_mem_chunks=num_mem_chunks)
+        for block in self.transformer_blocks:
+            src = block(src, train_test_split_index=train_test_split_index)
         # selects the target embeddings (B,num_targets,1,E)
-        output = output[:, single_eval_pos:, -1, :]
+        output = src[:, train_test_split_index:, -1, :]
         # runs the embeddings through the decoder to get
         # the logits of our predictions (B,num_targets,num_classes)
         output = self.decoder(output)
         return output
 
 
-# handle variable number of features in here?
 class FeatureEncoder(nn.Module):
     def __init__(self, embedding_size: int):
         """Creates the linear layer that we will use to embed our features."""
         super().__init__()
         self.linear_layer = nn.Linear(1, embedding_size)
 
-    def forward(self, x: torch.Tensor, single_eval_pos: int) -> torch.Tensor:
+    def forward(self, x: torch.Tensor, train_test_split_index: int) -> torch.Tensor:
         """
         Normalizes all the features based on the mean and std of the features of the training data,
         clips them between -100 and 100, then applies a linear layer to embed the features.
 
         Args:
             x: (torch.Tensor) a tensor of shape (batch_size, num_rows, num_features)
-            single_eval_pos: (int) the number of datapoints in X_train
+            train_test_split_index: (int) the number of datapoints in X_train
         Returns:
             (torch.Tensor) a tensor of shape (batch_size, num_rows, num_features, embedding_size), representing
                            the embeddings of the features
         """
         x = x.unsqueeze(-1)
-        mean = torch.mean(x[:, :single_eval_pos], dim=1, keepdims=True)
-        std = torch.std(x[:, :single_eval_pos], dim=1, keepdims=True) + 1e-8
+        mean = torch.mean(x[:, :train_test_split_index], dim=1, keepdims=True)
+        std = torch.std(x[:, :train_test_split_index], dim=1, keepdims=True) + 1e-8 #TODO: maybe change the constant
         x = (x - mean) / std
         x = torch.clip(x, min=-100, max=100)
         return self.linear_layer(x)
@@ -141,37 +141,6 @@ class TargetEncoder(nn.Module):
         return self.linear_layer(y)
 
 
-class TransformerEncoderStack(nn.Module):
-    def __init__(self, num_layers: int, embedding_size: int, num_attention_heads: int, mlp_hidden_size: int):
-        """
-        Instantiates num_layers many Transformer Blocks and stores them in a list so we can use
-        them in the forward
-        """
-        super().__init__()
-        self.transformer_blocks = nn.ModuleList()
-        for _ in range(num_layers):
-            self.transformer_blocks.append(
-                TransformerEncoderLayer(embedding_size, num_attention_heads, mlp_hidden_size)
-            )
-
-    def forward(self, x: torch.Tensor, single_eval_position: int, num_mem_chunks: int = 1) -> torch.Tensor:
-        """
-        Takes the embeddings of all the cells of the table as input and applies num_layers many Transformer blocks.
-
-        Args:
-            x: (torch.Tensor) a tensor of shape (batch_size, num_rows, num_features, embedding_size)
-                              that contains all the embeddings for all the cells in the table
-            single_eval_position: (int) the length of X_train
-            num_mem_chunks: (int) Number of chunks that memory-intense operations will be split into.
-                                  Higher values use less memory but are slower. Needs to be set to 1
-                                  during training to get correct gradients.
-
-        Returns
-            (torch.Tensor) a tensor of shape (batch_size, num_rows, num_features, embedding_size)
-        """
-        for block in self.transformer_blocks:
-            x = block(x, single_eval_position=single_eval_position, num_mem_chunks=num_mem_chunks)
-        return x
 
 
 class TransformerEncoderLayer(nn.Module):
@@ -204,7 +173,7 @@ class TransformerEncoderLayer(nn.Module):
         self.norm2 = LayerNorm(embedding_size, eps=layer_norm_eps, device=device, dtype=dtype)
         self.norm3 = LayerNorm(embedding_size, eps=layer_norm_eps, device=device, dtype=dtype)
 
-    def forward(self, src: torch.Tensor, single_eval_position: int, num_mem_chunks: int = 1) -> torch.Tensor:
+    def forward(self, src: torch.Tensor, train_test_split_index: int, num_mem_chunks: int = 1) -> torch.Tensor:
         """
         Takes the embeddings of the table as input and applies self-attention between features
         and self-attention between datapoints followed by a simple 2 layer MLP.
@@ -212,7 +181,7 @@ class TransformerEncoderLayer(nn.Module):
         Args:
             src: (torch.Tensor) a tensor of shape (batch_size, num_rows, num_features, embedding_size)
                                 that contains all the embeddings for all the cells in the table
-            single_eval_position: (int) the length of X_train
+            train_test_split_index: (int) the length of X_train
             num_mem_chunks: (int) Number of chunks that memory-intense operations will be split into.
                                   Higher values use less memory but are slower. Needs to be set to 1
                                   during training to get correct gradients.
@@ -236,12 +205,17 @@ class TransformerEncoderLayer(nn.Module):
 
         @memory_chunking(num_mem_chunks)
         def datapoint_attention(x):
+            # training data attends to itself
             x_left = self.self_attention_between_datapoints(
-                x[:, :single_eval_position], x[:, :single_eval_position], x[:, :single_eval_position]
+                x[:, :train_test_split_index],
+                x[:, :train_test_split_index],
+                x[:, :train_test_split_index],
             )[0]
             # test data attends to the training data
             x_right = self.self_attention_between_datapoints(
-                x[:, single_eval_position:], x[:, :single_eval_position], x[:, :single_eval_position]
+                x[:, train_test_split_index:],
+                x[:, :train_test_split_index],
+                x[:, :train_test_split_index],
             )[0]
             return torch.cat([x_left, x_right], dim=1) + x
 
